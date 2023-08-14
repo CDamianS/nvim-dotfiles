595cedaaa2e7c953ac2d576624ed66bc26b0c786
CDamianS <A01656277@tec.mx>
2023-08-14 16:38:07 -0600

    dotfiles

---
+++ lua/main/remap.lua
@@ -0,0 +1,5 @@
+vim.g.mapleader=" "
+vim.keymap.set("n", "<leader>op", vim.cmd.Ex)
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("n", "<leader>fs", vim.cmd.w)
+vim.keymap.set("n", "<leader>wq", vim.cmd.wq)
